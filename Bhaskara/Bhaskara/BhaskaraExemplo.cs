2cc44f202a2d590e3244a4cc1c8a8b121d20d9c9
Alice Janatto <janattoa@gmail.com>
2024-09-20 08:35:26 -0300

    teste Bhaskara

---
+++ Bhaskara/Bhaskara/BhaskaraExemplo.cs
@@ -0,0 +1,22 @@
+﻿namespace Bhaskara
+{
+    public static class BhaskaraExemplo
+    {
+        public static (double, double) CalcularRaizes(double a, double b, double c)
+        {
+            double delta = b * b - 4 * a * c;
+
+            if (delta > 0)
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+                return x1 < x2 ? (x1, x2) : (x2, x1);
+            }
+            else
+            {
+                throw new InvalidOperationException("Sua equação não possui duas raízes reais!");
+            }
+        }
+    }
+}
